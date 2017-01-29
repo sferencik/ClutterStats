035662e571ebca184fcb015f379c70026a3ad4f1
sferencik <sferencik@gmail.com>
2017-01-29 11:28:31 +0100

    Fix version remarks

--- clutter-stats-common/src/main/java/sferencik/teamcity/clutterstats/Feature.java
+++ clutter-stats-common/src/main/java/sferencik/teamcity/clutterstats/Feature.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Feature {
     /** Is the feature set up to measure the disk size at the beginning of the build?
      * @param params the parameter map from the build feature
-     * @return true/false based on the radio-box selection; false even if none of the radio options is selected (which happens after upgrade from plugin versions before 3.0.0)
+     * @return true/false based on the radio-box selection; false even if none of the radio options is selected (which happens after upgrade from plugin versions before 2.0.0)
      */
     static boolean isMeasureBeforeBuild(Map<String, String> params) {
         Names names = new Names();
@@ -21,7 +21,7 @@ public class Feature {
         return whenToMeasure == null ? false : whenToMeasure.equals(names.getAfterBuild());
     }
 
-    /** Is the "when to measure" radio box checked one way or another? It may not be if someone set up the ClutterStats build feature before v3.0.0.
+    /** Is the "when to measure" radio box checked one way or another? It may not be if someone set up the ClutterStats build feature before v2.0.0.
      *
      * @param params the parameter map from the build feature
      * @return true if the radio is unchecked
